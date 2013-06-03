f86890d18c1ad3b8663dbd83a9af9142f0e4569f
Josh Wymer <wymetyme@gmail.com>
2013-06-03 01:53:53 -0700

    fixing package paths that were broken from refactor

--- base/settings.py
+++ base/settings.py
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'base.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'base.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
